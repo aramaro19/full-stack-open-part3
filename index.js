46d373974c1af86c04d7a26e01cb2450b34f0281
strebor19 <arobertamaro@gmail.com>
2023-08-05 18:32:39 -0400

    install cors module

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 morgan.token('data', (request, response) => JSON.stringify(request.body))
 
 app.use(express.json())
 app.use(morgan(':method :status :res[content-length] - :response-time ms :data'))
+app.use(cors)
 
 let persons = [
     {
