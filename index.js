615aa7e7945becc72e73ad85269b3220149e379a
strebor19 <arobertamaro@gmail.com>
2023-08-08 05:54:21 -0400

    do 3.15

--- index.js
+++ index.js
@@ -1,69 +1,61 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = express()
-morgan.token('data', (request, response) => JSON.stringify(request.body))
-const Person = require('./models/person')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+morgan.token('data', (request, response) => JSON.stringify(request.body));
+const Person = require('./models/person');
 
-app.use(express.json())
-app.use(morgan(':method :status :res[content-length] - :response-time ms :data'))
-app.use(cors())
-
+app.use(express.json());
+app.use(morgan(':method :status :res[content-length] - :response-time ms :data'));
+app.use(cors());
 
 app.get('/', (request, response) => {
-	response.send('<h1>Phonebook</h1>')
-})
+	response.send('<h1>Phonebook</h1>');
+});
 
 app.get('/api/persons/', (request, response) => {
-	Person
-		.find({})
-		.then(people => {
-			response.json(people)
-		})
-})
+	Person.find({}).then(people => {
+		response.json(people);
+	});
+});
 
 app.get('/api/persons/:id', (request, response) => {
-	Person	
-		.findById(request.params.id)
-		.then(person => {
-			response.json(person)
-		})
-})
+	Person.findById(request.params.id).then(person => {
+		response.json(person);
+	});
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
-
-	response.status(204).end()
-})
+app.delete('/api/persons/:id', (request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then(response.status(204).end())
+		.catch(error => next(error));
+});
 
 app.post('/api/persons/', (request, response) => {
-	const body = request.body
+	const body = request.body;
 	if (!body.name || !body.number) {
 		return response.status(400).json({
 			error: 'content missing',
-		})
-	} 
-	
+		});
+	}
+
 	const person = new Person({
 		name: body.name,
 		number: body.number,
-	})
+	});
 
-	person	
-		.save()
-		.then(savedPerson => {
-			response.json(savedPerson)
-		})
-})
+	person.save().then(savedPerson => {
+		response.json(savedPerson);
+	});
+});
 
 const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
+	response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
-})
+	console.log(`Server running on port ${PORT}`);
+});
--- models/person.js
+++ models/person.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to:', url)
+console.log('connecting to:', url);
 
 mongoose
 	.connect(url)
 	.then(result => {
-		console.log('connected to', url)
+		console.log('connected to mongoDB');
 	})
 	.catch(error => {
-		console.log('error connecting to mongoDB:', error.message)
-	})
+		console.log('error connecting to mongoDB:', error.message);
+	});
 
 const personSchema = new mongoose.Schema({
 	name: String,
 	number: String,
-})
+});
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
