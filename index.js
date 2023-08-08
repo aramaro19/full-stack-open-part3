36723f2c3e90b55b065a784c1445181bcd4720e1
strebor19 <arobertamaro@gmail.com>
2023-08-08 07:01:49 -0400

    do 3.17

--- index.js
+++ index.js
@@ -62,6 +62,19 @@ app.post('/api/persons/', (request, response) => {
 	});
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+	const body = request.body;
+	const person = {
+		name: body.name,
+		number: body.number,
+	};
+	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+		.then(updatedNote => {
+			response.json(updatedNote);
+		})
+		.catch(error => next(error));
+});
+
 const errorHandler = (error, request, response, next) => {
 	console.error(error.message);
 
